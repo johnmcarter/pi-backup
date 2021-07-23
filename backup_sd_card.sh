dde45f62e29f4913862ecb07c7f4d750dcf8a57a
John Carter <jmcarter211@gmail.com>
2021-07-23 09:40:11 -0400

    added copying to disk drive

--- backup_sd_card.sh
+++ backup_sd_card.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Created: 2021/07/23 07:26:18
-# Last modified: 2021/07/23 08:09:32
-# Copy data from Raspberry Pi SD card to local directory
+# Last modified: 2021/07/23 09:32:29
+# Copy data from Raspberry Pi SD card 
+# to local machine and backup disk
 
 red=$'\e[1;31m'
 grn=$'\e[1;32m'
 end=$'\e[0m'
 
 date=$(date -I)
+local_of="/home/birch/pi_data/backups/$1/$date.img.gz"
+disk_of="/media/birch/Seagate Backup Plus Drive/backups/$1/$date.img.gz"
+disk_dir="/media/birch/Seagate Backup Plus Drive/backups/$1/"
 
 exit_process() {
     echo "[${red}EXIT${end}] Killing script..."
@@ -22,8 +26,20 @@ if [[ $# -ne 1 ]]; then
     exit_process
 fi
 
-echo "[${grn}INFO${end}] Copying and compressing $1 SD card..."
+# copy locally 
+if [ ! -e "$local_of" ]; then
+    echo "[${grn}INFO${end}] Copying and compressing $1 SD card to $local_of"
+    sudo dd bs=4M if=/dev/mmcblk0 status=progress conv=fsync | gzip > $local_of
+else 
+    echo "[${grn}INFO${end}] Local file $local_of exists. Skipping copy..."
+fi
 
-sudo dd bs=4M if=/dev/mmcblk0 status=progress conv=fsync | gzip > /home/birch/pi_data/backups/$1-$date.img.gz
+# copy to backup drive
+if [ ! -e "$disk_of" ]; then
+    echo "[${grn}INFO${end}] Copying $local_of to $disk_dir"
+    cp "$local_of" "$disk_dir"
+else 
+    echo "[${grn}INFO${end}] Disk file $disk_of exists. Skipping copy..."
+fi
 
-echo "[${grn}INFO${end}] Success"
+echo "[${grn}INFO${end}] Backup of $1 complete"
